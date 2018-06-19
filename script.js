98dd4c27770245fb91f4e4c1e2817135867efb80
PawelLyko <pawel_lyko@yahoo.com>
2018-06-19 11:21:47 -0400

    plzwork

---
+++ script.js
@@ -0,0 +1,19 @@
+
+$(".signUpBtn").click(function(){
+
+$("#modalForm").css("display", "block")
+
+})
+
+
+
+$(".loginBtn").click(function(){
+
+$("#loginForm").css("display", "block")
+
+})
+
+
+
+
+
